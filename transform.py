90ee70af356e40773f1021aebbdf629f05ca7a68
greekinRoma <zz8843157@gmail.com>
2026-04-10 14:00:26 +0800

    exp2

--- transform.py
+++ transform.py
@@ -13,7 +13,7 @@ from dataset import DroneDataset
 def get_args():
     parser = argparse.ArgumentParser(description="Road Segmentation Training")
     parser.add_argument('--lr', type=float, default=0.0001, help='learning rate')
-    parser.add_argument('--batch_size', type=int, default=4, help='input batch size')
+    parser.add_argument('--batch_size', type=int, default=1, help='input batch size')
     parser.add_argument('--epochs', type=int, default=100, help='number of epochs')
     parser.add_argument('--train_dir', type=str, default='datasets/train', help='directory of training input images')
     parser.add_argument('--test_dir', type=str, default='datasets/test', help='directory of testing input images')
@@ -21,8 +21,8 @@ def get_args():
     parser.add_argument('--num_head',type=int, default=8, help='number of attention heads')
     parser.add_argument('--embed_dim', type=int, default=768, help='dimension of patch embedding')
     parser.add_argument('--model_save_dir', type=str, default='save', help='path to save the trained model')
-    parser.add_argument('--m', type=int, default=2, help='number of rows to divide the image into')
-    parser.add_argument('--n', type=int, default=2, help='number of columns to divide the image into')
+    parser.add_argument('--m', type=int, default=1, help='number of rows to divide the image into')
+    parser.add_argument('--n', type=int, default=1, help='number of columns to divide the image into')
     return parser.parse_args()
 
 def my_collate_fn(batch):
