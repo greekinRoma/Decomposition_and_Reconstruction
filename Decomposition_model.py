0faaacc4c901d2b60d7e8971877a631e991fdb22
greekinRoma <zz8843157@gmail.com>
2026-04-15 14:55:19 +0800

    exp11

--- Decomposition_model.py
+++ Decomposition_model.py
@@ -17,23 +17,21 @@ class DecompositionModel(nn.Module):
         self.origin_patch_size = origin_patch_size
         self.m = m
         self.n = n
+        self.num_heads = num_head
         self.original_patch_embed = nn.Conv2d(in_channels=3,out_channels=self.origin_embed_dim,kernel_size=self.origin_patch_size,stride=self.origin_patch_size,groups=3).cuda()
         self.resize_patch_embed = nn.Conv2d(in_channels=3,out_channels=self.resize_embed_dim,kernel_size=self.resize_patch_size,stride=self.resize_patch_size,groups=3).cuda()
         self.b = nn.Linear(self.origin_embed_dim, self.origin_embed_dim)
         self.q  = nn.Linear(self.resize_embed_dim, self.resize_embed_dim)
-        self.pos_embed = nn.Parameter(torch.zeros(1, self.resize_embed_dim, 1024 // self.resize_patch_size, 1024 // self.resize_patch_size),requires_grad=True)
+        self.pos_embed = nn.Parameter(torch.zeros(1, 1024 // self.resize_patch_size//self.m , 1024 // self.resize_patch_size//self.n, self.resize_embed_dim),requires_grad=True)
         self.norm_b = nn.LayerNorm(self.origin_embed_dim)
         self.norm_q = nn.LayerNorm(self.resize_embed_dim)
-        self.num_heads = num_head
         self.reszie = 1024 // self.resize_patch_size
         self.down = nn.Sequential(
             nn.Linear(self.resize_embed_dim//self.num_heads, self.origin_embed_dim//self.num_heads)
         )
         self.proj = nn.Linear(self.resize_embed_dim, self.resize_embed_dim)
         self.mat_mul = multiply_matrix_with_rope(num_heads=num_head,head_dim=self.origin_embed_dim//self.num_heads, rope_theta=10.0)
-        self.q = nn.Parameter(torch.rand(1,self.num_heads,1024 // self.resize_patch_size*1024 // self.resize_patch_size//self.m//self.n,self.resize_embed_dim//self.num_heads))
-        self.rw = 1024//self.resize_patch_size//self.n
-        self.rh = 1024//self.resize_patch_size//self.m
+
     def forward(self, x):
         B, C, H, W = x.shape
         resize_x = torch.nn.functional.interpolate(x, size=(1024, 1024), mode='bilinear', align_corners=False)
@@ -42,25 +40,25 @@ class DecompositionModel(nn.Module):
         x_pad = pad(x, (0, W_pad, 0, H_pad), mode='constant', value=0)
 
         origin_patches = self.original_patch_embed(x_pad).permute(0,2,3,1)
-        # resize_patches = (self.resize_patch_embed(resize_x)+ self.pos_embed).permute(0,2,3,1)
+        resize_patches = (self.resize_patch_embed(resize_x)).permute(0,2,3,1)
 
         origin_patches, [oh, ow] = window_partition(origin_patches, m=self.m, n=self.n)
-        # resize_patches, [rh, rw] = window_partition(resize_patches, m=self.m, n=self.n)
+        resize_patches, [rh, rw] = window_partition(resize_patches, m=self.m, n=self.n)
 
         origin_patches = self.norm_b(origin_patches)
-        # resize_patches = self.norm_q(resize_patches)
+        resize_patches = self.norm_q(resize_patches + self.pos_embed)
 
-        # q = self.q(resize_patches)
+        q = self.q(resize_patches)
         b = self.b(origin_patches)
 
         b = b.reshape(B*self.m*self.n, oh, ow, self.num_heads, self.origin_embed_dim // self.num_heads).permute(0,3,1,2,4).reshape(B*self.m*self.n, self.num_heads, oh * ow, self.origin_embed_dim // self.num_heads)
-        # q = q.reshape(B*self.m*self.n, rh, rw, self.num_heads, self.resize_embed_dim // self.num_heads).permute(0,3,1,2,4).reshape(B*self.m*self.n, self.num_heads, rh * rw, self.resize_embed_dim // self.num_heads)
+        q = q.reshape(B*self.m*self.n, rh, rw, self.num_heads, self.resize_embed_dim // self.num_heads).permute(0,3,1,2,4).reshape(B*self.m*self.n, self.num_heads, rh * rw, self.resize_embed_dim // self.num_heads)
 
         tmp_b = b.clone()
-        q = torch.nn.functional.normalize(self.q.expand(self.m*self.n, -1, -1, -1), dim=-1)
+        q = torch.nn.functional.normalize(q, dim=-1)
         b = torch.nn.functional.normalize(b, dim=-1)
 
-        attn = self.mat_mul.multiply(q=q, b=b, end_x_xq=self.rw, end_y_xq=self.rh, end_x_xb=ow, end_y_xb=oh)
+        attn = self.mat_mul.multiply(q=q, b=b, end_x_xq=rw, end_y_xq=rh, end_x_xb=ow, end_y_xb=oh)
         x = (attn @ tmp_b).reshape(B, self.m, self.n, self.num_heads, self.reszie//self.m, self.reszie//self.n, -1).permute(0, 1, 4, 2, 5, 3, 6).reshape(B, self.reszie, self.reszie, self.resize_embed_dim)
 
         x = self.proj(x).permute(0, 3, 1, 2)
